25c132c6715db05b054dd0340884139fced26751
studiareCS <yamil.aucca.q@gmail.com>
2024-11-24 22:46:08 -0500

    feat: Funcion para preprocesar los datos MNIST para una MLP
    Se creo una funcion para realizar un preprocesamiento de las imagenes MNIST de entrenamiento y testeo para una MLP. Se realiza un aplanado a las imagenes de 28x28 a un np.array de una dimension (de tipo float32) con 784 valores (correspondientes a cada pixel de una imagen), cada valor es dividido por 255.0 para pasar a cada valor a un rango de 0 a 1 (normalizado)

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,9 @@
 import numpy as np
+
+def preprocess_data_for_mlp(x_train, x_test):
+    """Realiza el preprocesamiento para la MLP (aplanado de imágenes)."""
+    # Aplanar las imágenes de 28x28 a 784 (28*28) y normalizar los valores (0 a 255) a un rango de 0 a 1
+    x_train_mlp = x_train.reshape(x_train.shape[0], -1).astype('float32') / 255.0
+    x_test_mlp = x_test.reshape(x_test.shape[0], -1).astype('float32') / 255.0
+
+    return x_train_mlp, x_test_mlp
