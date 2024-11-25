846cd86b4f080f4f8d4c5bb25d0188d26da4aace
studiareCS <yamil.aucca.q@gmail.com>
2024-11-24 21:57:44 -0500

    feat: Cargado de imagenes y etiquetas del directorio data/raw
    Se agrego el cargado de los archivos raw (imagenes y etiquetas del directorio data/raw) cambiando su tipo a np.uint8 y una modificacion al tamaño de cada np.array (28 x 28 para las imagenes; 1 dimension para las etiquetas)

--- scripts/data_pipeline.py
+++ scripts/data_pipeline.py
@@ -29,7 +29,14 @@ def load_data():
     """Carga los datos de MNIST en formato raw"""
     raw_dir = os.path.join('.', 'data', 'raw')
 
+    # Rutas de los archivos raw 
     train_images_file = os.path.join(raw_dir, 'train_images.raw')
     train_labels_file = os.path.join(raw_dir, 'train_labels.raw')
     test_images_file = os.path.join(raw_dir, 'test_images.raw')
     test_labels_file = os.path.join(raw_dir, 'test_labels.raw')
+
+    # Cargar imagenes y etiquetas desde los archivos raw 
+    train_images = np.fromfile(train_images_file, dtype=np.uint8).reshape(-1, 28, 28)
+    train_labels = np.fromfile(train_labels_file, dtype=np.uint8)
+    test_images = np.fromfile(test_images_file, dtype=np.uint8).reshape(-1, 28, 28)
+    test_labels = np.fromfile(test_labels_file, dtype=np.uint8)
