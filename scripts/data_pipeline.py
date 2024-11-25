8b86f95c948f09c16930199097247f88c0497b5a
studiareCS <yamil.aucca.q@gmail.com>
2024-11-24 19:52:38 -0500

    feat: Descarga del dataset MNIST
    Se descarga MNIST mediante TensorFlow y se divide en 4 np.array (x_train, x_test contienen las imagenes; y_train, y_test contienen las etiquetas)

--- scripts/data_pipeline.py
+++ scripts/data_pipeline.py
@@ -2,4 +2,11 @@ import tensorflow as tf
 import numpy as np
 import os
 
+def download_raw_data():
+    """Descarga MNIST original y lo guarda en /data/raw en formato raw"""
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+    
+    
+    
