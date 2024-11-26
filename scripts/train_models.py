0496fce92987c35b3c51840a3bfbd99eadb67e7d
studiareCS <yamil.aucca.q@gmail.com>
2024-11-26 02:12:26 -0500

    feat: Se agrega el entrenamiento y guardado del MLP a la funcion
    Se realiza el entrenamiento del modelo MLP especificando el parametro de las epochs y el batch_size a considerar durante el entrenamiento (teniendo en cuenta la gran cantidad de datos que posee MNIST, 60000 imagenes para el entrenamiento). Luego, se guarda el modelo ya entrenado en la carpeta models/ (se crea si es que no existe)

--- scripts/train_models.py
+++ scripts/train_models.py
@@ -22,7 +22,7 @@ def load_processed_data(model_type):
     return x_train, x_test, y_train, y_test
 
 def train_mlp():
-    """Entrena un modelo de red neuronal multicapa (MLP) para MNIST"""
+    """Entrena un modelo de red neuronal multicapa (MLP) para MNIST y lo guarda"""
     # Cargar los datos preprocesados para la MLP
     x_train, x_test, y_train, y_test = load_processed_data('mlp')
 
@@ -33,3 +33,11 @@ def train_mlp():
         loss='categorical_crossentropy', 
         metrics=['accuracy']
     )
+
+    # Entrenar el modelo MLP
+    mlp_model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+
+    # Guardar el modelo entrenado
+    os.makedirs(os.path.join('.', 'models'), exist_ok=True)
+    mlp_model.save(os.path.join('.', 'models', 'mlp_model.h5'))
+    print("Modelo MLP entrenado y guardado.")
