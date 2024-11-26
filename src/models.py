ae69dd879efaa1a0f60ff3ae34c542368b980a80
studiareCS <yamil.aucca.q@gmail.com>
2024-11-25 20:35:21 -0500

    feat: Funcion para crear un modelo MLP
    Se crea una funcion donde se crea un modelo MLP con 3 hidden layers y funciones de activacion ReLU, mientras que para la output layer se utiliza la funcion softmax, la input layer contiene 784 valores para almacenar el valor contenido por cada pixel de una imagen en MNIST (28x28 = 784 pixeles)

--- src/models.py
+++ src/models.py
@@ -1,2 +1,13 @@
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+
+def build_mlp_model():
+    """Construye un modelo de red neuronal multicapa (MLP)"""
+    model = Sequential([
+        Flatten(input_shape=(784, )),
+        Dense(256, activation='relu'),
+        Dense(128, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+    return model
