5763f8ae9371e477a53c291b62ab39f6dd73dfab
studiareCS <yamil.aucca.q@gmail.com>
2024-11-26 01:22:56 -0500

    feat: Funcion para cargar las imagenes y etiquetas preprocesadas de MNIST
    Se crea una funcion para cargar las imagenes y etiquetas preprocesadas de MNIST segun el tipo de modelo especificado (pues varia el formato de las imagenes para el entrenamiento y testeo, para las etiquetas no hay diferencias)

--- scripts/train_models.py
+++ scripts/train_models.py
@@ -2,3 +2,21 @@ import os
 import numpy as np
 from tensorflow.keras.optimizers import Adam
 from src.models import build_mlp_model, build_cnn_model
+
+def load_processed_data(model_type):
+    """Carga los datos procesados de MNIST"""
+    processed_dir = os.path.join('.', 'data', 'processed')
+
+    # Cargar las imagenes preprocesados de MNIST segun el tipo de modelo
+    if(model_type == 'mlp'):
+        x_train = np.load(os.path.join(processed_dir, 'train_mlp.npy'))
+        x_test = np.load(os.path.join(processed_dir, 'test_mlp.npy'))
+    elif(model_type == 'cnn'):
+        x_train = np.load(os.path.join(processed_dir, 'train_cnn.npy'))
+        x_test = np.load(os.path.join(processed_dir, 'test_cnn.npy'))
+
+    # Cargar las etiquetas preprocesadas de MNIST
+    y_train = np.load(os.path.join(processed_dir, 'train_labels.npy'))
+    y_test = np.load(os.path.join(processed_dir, 'test_labels.npy'))
+
+    return x_train, x_test, y_train, y_test
