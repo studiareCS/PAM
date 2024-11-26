92385e457dab65a2f7bbe058837e704e4b3ddbc8
studiareCS <yamil.aucca.q@gmail.com>
2024-11-26 00:55:48 -0500

    feat: Funcion para crear un modelo CNN
    Se crea una funcion para construir un modelo CNN que tiene la siguiente estructura: Una input layer (28, 28, 1), una capa convolucional (32 filtros y kernel 3x3), una capa de maxpooling (kernel 2x2), otra capa convolucional (64 filtros y kernel 3x3), otra capa de maxpooling (kernel 2x2) y de ahi una capa de aplanamiento, una densa (128 neuronas) y la output layer

--- src/models.py
+++ src/models.py
@@ -3,11 +3,24 @@ from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 
 def build_mlp_model():
     """Construye un modelo de red neuronal multicapa (MLP)"""
-    model = Sequential([
+    mlp_model = Sequential([
         Flatten(input_shape=(784, )),
         Dense(256, activation='relu'),
         Dense(128, activation='relu'),
         Dense(64, activation='relu'),
         Dense(10, activation='softmax')
     ])
-    return model
+    return mlp_model
+
+def build_cnn_model():
+    """Construye un modelo de red neuronal convolucional (CNN)"""
+    cnn_model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(10, activation='softmax')
+    ])
+    return cnn_model
