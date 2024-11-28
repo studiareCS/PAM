c7aacbe91e87e96ba4453096b7a71452273d7b00
studiareCS <yamil.aucca.q@gmail.com>
2024-11-27 22:36:23 -0500

    refactor: Utilizar config.yaml en models.py
    Se utiliza el archivo config.yaml en models.py para utilizar las configuraciones con respecto a las funciones de activacion. Se importan los modulos necesarios para ello: yaml, os

--- src/models.py
+++ src/models.py
@@ -1,26 +1,35 @@
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+import yaml
+import os
+
+# Abrir el archivo de configuracion
+with open(os.path.join('.', 'config', 'config.yaml'), 'r') as file:
+    config = yaml.safe_load(file)
 
 def build_mlp_model():
     """Construye un modelo de red neuronal multicapa (MLP)"""
     mlp_model = Sequential([
         Flatten(input_shape=(784, )),
-        Dense(256, activation='relu'),
-        Dense(128, activation='relu'),
-        Dense(64, activation='relu'),
-        Dense(10, activation='softmax')
+        Dense(256, activation=config['activation-functions']['relu']),
+        Dense(128, activation=config['activation-functions']['relu']),
+        Dense(64, activation=config['activation-functions']['relu']),
+        Dense(10, activation=config['activation-functions']['softmax'])
     ])
     return mlp_model
 
 def build_cnn_model():
     """Construye un modelo de red neuronal convolucional (CNN)"""
     cnn_model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        Conv2D(32, (3, 3), activation=config['activation-functions']['relu'], input_shape=(28, 28, 1)),
         MaxPooling2D((2, 2)),
-        Conv2D(64, (3, 3), activation='relu'),
+        Conv2D(64, (3, 3), activation=config['activation-functions']['relu']),
         MaxPooling2D((2, 2)),
         Flatten(),
-        Dense(128, activation='relu'),
-        Dense(10, activation='softmax')
+        Dense(128, activation=config['activation-functions']['relu']),
+        Dense(10, activation=config['activation-functions']['softmax'])
     ])
     return cnn_model
+
+# Cerrar el archivo de configuracion
+file.close()
