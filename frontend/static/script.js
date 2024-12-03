b3e3e1b31035b23c7a81bb4fc72ee94e64bbe6c8
studiareCS <yamil.aucca.q@gmail.com>
2024-12-02 23:33:15 -0500

    feat: Se agrega el dibujado en el lienzo
    Se agregan los eventos y condiciones necesarias para poder dibujar en el lienzo con el puntero del mouse

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -8,3 +8,24 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = "black";
 ctx.lineWidth = 15;
 ctx.lineCap = "round";
+
+// Dibujar en el lienzo
+let isDrawing = false;
+
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (isDrawing) {
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    }
+});
+
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
