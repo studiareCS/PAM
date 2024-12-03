ad20e69fdd3ca143d1f1448db26060a6e035d350
studiareCS <yamil.aucca.q@gmail.com>
2024-12-02 23:28:49 -0500

    feat: Se agrega la configuracion del lienzo
    Se agrega la configuracion del lienzo (color del fondo, marco, forma, etc.)

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -1,3 +1,10 @@
 // Obtener el canvas y el contexto
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+// Configuración del lienzo
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = "black";
+ctx.lineWidth = 15;
+ctx.lineCap = "round";
