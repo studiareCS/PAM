657c0315563e2c4352b8bbc262cb71c37ade9ec6
studiareCS <yamil.aucca.q@gmail.com>
2024-12-04 00:06:57 -0500

    feat: Se agregan las rutas de ubicacion de los modelos
    Se crea una instancia de la aplicacion Flask y luego se guardan las rutas a los modelos ya entrenados (MLP y CNN)

--- backend/app.py
+++ backend/app.py
@@ -3,3 +3,10 @@ import numpy as np
 import tensorflow as tf
 from PIL import Image
 import os
+
+app = Flask(__name__)
+
+# Ruta a los modelos entrenados
+MODEL_DIR = os.path.join(os.path.dirname(__file__), '../models')
+cnn_model_path = os.path.join(MODEL_DIR, 'cnn_model.h5')
+mlp_model_path = os.path.join(MODEL_DIR, 'mlp_model.h5')
