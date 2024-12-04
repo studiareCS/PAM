f5a14c86fbb5cda42986bde443e7724143daff4f
studiareCS <yamil.aucca.q@gmail.com>
2024-12-04 00:12:48 -0500

    feat: Se agregan el cargado de los modelos ya entrenados
    Se agrega el cargado de los modelos ya entrenados en formato .h5

--- backend/app.py
+++ backend/app.py
@@ -10,3 +10,7 @@ app = Flask(__name__)
 MODEL_DIR = os.path.join(os.path.dirname(__file__), '../models')
 cnn_model_path = os.path.join(MODEL_DIR, 'cnn_model.h5')
 mlp_model_path = os.path.join(MODEL_DIR, 'mlp_model.h5')
+
+# Cargar modelos
+cnn_model = tf.keras.models.load_model(cnn_model_path)
+mlp_model = tf.keras.models.load_model(mlp_model_path)
